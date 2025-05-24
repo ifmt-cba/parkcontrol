0296ebf10e016175858a6c372ef831ede84a7ac8
Fabio Junior <fabbhio007@gmail.com>
2025-05-23 22:45:00 -0400

    feat: implement user authentication with role-based dashboards and custom user model

--- parkcontrol/apps/usuarios/admin.py
+++ parkcontrol/apps/usuarios/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+# apps/usuarios/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario
+
+class UsuarioAdmin(UserAdmin):
+    model = Usuario
+    list_display = ('username', 'email', 'first_name', 'last_name', 'perfil_acesso', 'is_staff')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Informações de Perfil', {'fields': ('perfil_acesso',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Informações de Perfil', {'fields': ('perfil_acesso',)}),
+    )
+
+admin.site.register(Usuario, UsuarioAdmin)
--- parkcontrol/apps/usuarios/views.py
+++ parkcontrol/apps/usuarios/views.py
@@ -4,10 +4,18 @@ from django.contrib.auth.models import User # Import User model
 from django.contrib.auth import authenticate,logout,login # Import authenticate, logout, and login functions
 from django.contrib.auth.decorators import login_required # Import login_required decorator
 from django.contrib import messages # Import messages framework for displaying messages
+from django.contrib.auth.decorators import user_passes_test # Import user_passes_test decorator
+
 
 # Redirect to login page
 def redirect_to_login(request):
     return redirect('login_parkcontrol')  
+
+'''
+
+    AUTENTICATION 
+
+'''
 
 # Login page
 def login_parkcontrol(request):
@@ -22,7 +30,18 @@ def login_parkcontrol(request):
         # Check if the user is authenticated
         if user is not None:
             login(request, user)
-            return redirect('home_parkcontrol') # Redirect to home page after login
+             # Redirecionamento por perfil
+            perfil = getattr(user, 'perfil_acesso', None)
+
+            if perfil == 'Administrador':
+                return redirect('dashboard_administrador')
+            elif perfil == 'Frentista':
+                return redirect('dashboard_frentista')
+            elif perfil == 'Contador':
+                return redirect('dashboard_contador')
+            else:
+                messages.warning(request, 'Perfil de acesso não identificado.')
+                return redirect('login_parkcontrol')
         else:
             messages.error(request, 'E-mail ou senha inválidos.')
             return render(request, 'autenticacao/login.html')
@@ -58,7 +77,44 @@ def logout_parkcontrol(request):
     logout(request)    
     return render(request, 'autenticacao/logout.html')
 
-# Home page
-@login_required
-def home_parkcontrol(request):
+
+'''
+
+    USER ACCESS CONTROL
+
+'''
+
+def is_administrador(user):
+    return user.is_authenticated and getattr(user, 'perfil_acesso', None) == 'Administrador'
+
+def is_contador(user):
+    return user.is_authenticated and getattr(user, 'perfil_acesso', None) == 'Contador'
+
+def is_frentista(user):
+    return user.is_authenticated and getattr(user, 'perfil_acesso', None) == 'Frentista'
+
+
+
+'''
+
+    DASHBOARD
+
+'''
+
+# Dashboard for Administrador
+@login_required(login_url='login_parkcontrol')
+@user_passes_test(is_administrador, login_url='login_parkcontrol')
+def dashboard_administrador(request):
     return render(request, 'usuarios/administrador/dashboard_administrador.html')
+
+# Dashboard for Contador
+@login_required(login_url='login_parkcontrol')
+@user_passes_test(is_contador, login_url='login_parkcontrol')
+def dashboard_contador(request):
+    return render(request, 'usuarios/contador/dashboard_contador.html')
+
+# Dashboard for Frentista
+@login_required(login_url='login_parkcontrol')
+@user_passes_test(is_frentista, login_url='login_parkcontrol')
+def dashboard_frentista(request):
+    return render(request, 'usuarios/frentista/dashboard_frentista.html')
