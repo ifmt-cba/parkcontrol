7572420bb4633682736dfb58e6d06d31b19df240
Emmylly Oliveira <m.emmylly14@gmail.com>
2025-06-23 16:46:33 -0400

    .

--- parkcontrol/apps/pagamentos/admin.py
+++ parkcontrol/apps/pagamentos/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from apps.pagamentos.models import CobrancaDiaria
+from apps.pagamentos.models import CobrancaDiaria, CobrancaMensalista
 
 admin.site.register(CobrancaDiaria)
+admin.site.register(CobrancaMensalista)  # Assuming the model is named CobrancaMensalista
