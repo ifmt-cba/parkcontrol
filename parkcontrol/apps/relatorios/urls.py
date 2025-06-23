4f9c305d8a8d8b23fff915a139b23e18fa14c141
Fabio Junior <fabbhio007@gmail.com>
2025-06-23 18:14:56 -0400

    fix: update links in relatorios templates for correct navigation

--- parkcontrol/apps/relatorios/urls.py
+++ parkcontrol/apps/relatorios/urls.py
@@ -5,7 +5,7 @@ app_name = 'relatorios'
 
 urlpatterns = [
     path('', views.relatorios, name='dashboard_relatorios'),
-    path('', views.listar_relatorios, name='listar'),
+    path('listar/', views.listar_relatorios, name='listar'),
     path('criar/', views.criar_relatorio, name='criar'),
     path('visualizar/<int:id>/', views.visualizar_relatorio, name='visualizar'),
     path('editar/<int:id>/', views.editar_relatorio, name='editar'),
