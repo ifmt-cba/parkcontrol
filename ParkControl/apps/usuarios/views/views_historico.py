faeb8b6eee49e5222de7d2a5f426495e290ff769
Emmylly Oliveira <m.emmylly14@gmail.com>
2025-06-29 22:08:35 -0400

    feat(#177): Criaçao de logging

--- ParkControl/apps/usuarios/views/views_historico.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from apps.vagas.models import Vaga
-
-def historico(request):
-    return render(request, 'usuarios/administrador/historico/dashboard_historico.html')
-
-def historico_vagas(request):
-    historicos = Vaga.historico.all().order_by('-history_date')
-    return render(request, 'usuarios/administrador/historico/historico_vagas.html', {'historicos': historicos})
