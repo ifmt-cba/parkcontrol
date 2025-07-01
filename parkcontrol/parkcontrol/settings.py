e4df0c84c5a7732c4802d2152c831b09663c4406
Fabio Junior <fabbhio007@gmail.com>
2025-07-01 18:22:13 -0400

    Add SECURE_PROXY_SSL_HEADER setting for improved SSL handling

--- parkcontrol/parkcontrol/settings.py
+++ parkcontrol/parkcontrol/settings.py
@@ -151,6 +151,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'noreply@parkcontrol.com'
 
 SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
